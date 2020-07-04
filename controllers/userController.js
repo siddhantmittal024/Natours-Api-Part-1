e5f207de20bc56ffa1bfcfde0108ce89697ce7eb
Siddhant Mittal <sidmit2001@gmail.com>
2020-07-04 19:39:44 +0530

    Add files via upload

---
+++ controllers/userController.js
@@ -0,0 +1,34 @@
+exports.getAllUsers = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'Route not defined!!',
+    });
+};
+
+exports.createUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'Route not defined!!',
+    });
+};
+
+exports.getUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'Route not defined!!',
+    });
+};
+
+exports.updateUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'Route not defined!!',
+    });
+};
+
+exports.deleteUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'Route not defined!!',
+    });
+};
